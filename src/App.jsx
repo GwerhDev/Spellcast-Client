46c2f0d3673fd796c38b5866314d6964cfe6b0d1
GwerhDev <gwerh.dev@gmail.com>
2025-05-10 22:25:16 -0400

    minimal useful

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { PDFReader } from './components/PDFReader'
+
+function App() {
+
+  return (
+    <main>
+      <h1>Spellcast</h1>
+      <PDFReader />
+    </main>
+  )
+}
+
+export default App
