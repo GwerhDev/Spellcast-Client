6ee5bfdd0f0b90da97a7245d1207a3b8c1706bc8
gerardo <gerardo@recemed.com>
2025-09-15 11:09:18 -0300

    simplifying ui

--- src/app/layouts/UserLayout.tsx
+++ src/app/layouts/UserLayout.tsx
@@ -1,32 +1,15 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { LogoutModal } from '../components/Modals/LogoutModal';
-import { useEffect, useState } from 'react';
 import { Browser } from '../components/Browser/Browser';
 
 export default function UserLayout() {
-  const location = useLocation();
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 1024);
-  const shouldHideBrowser = !isSmallScreen || location.pathname !== `/user`;
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth < 1024);
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
   return (
     <>
-      {
-        shouldHideBrowser &&
-        <div className="dashboard-sections">
-          <Browser>
-            <Outlet />
-          </Browser >
-        </div >
-      }
+      <div className="dashboard-sections">
+        <Browser>
+          <Outlet />
+        </Browser >
+      </div >
       <LogoutModal />
     </>
   );
