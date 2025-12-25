2a5a69208678854208d2cf1fab6e07505593168f
GwerhDev <gwerh.dev@gmail.com>
2025-12-25 15:27:07 -0300

    style: enhancing aesthetics

--- src/app/components/Selectors/InputTypeSelector.tsx
+++ src/app/components/Selectors/InputTypeSelector.tsx
@@ -10,28 +10,30 @@ interface InputTypeSelectorProps {
 
 export const InputTypeSelector: React.FC<InputTypeSelectorProps> = ({ inputType, setInputType, }) => {
   return (
-    <div className={s.tabs}>
-      <button
-        className={`${s.tab} ${s.left} ${inputType === 'upload' ? s.active : ''}`}
-        onClick={() => setInputType('upload')}
-      >
-        <FontAwesomeIcon icon={faUpload} />
-        <span className={s.title}>Upload</span>
-      </button>
-      <button
-        className={`${s.tab} ${s.middle} ${inputType === 'create' ? s.active : ''}`}
-        onClick={() => setInputType('create')}
-      >
-        <FontAwesomeIcon icon={faFileCirclePlus} />
-        <span className={s.title}>Create</span>
-      </button>
-      <button
-        className={`${s.tab} ${s.right} ${inputType === 'text' ? s.active : ''}`}
-        onClick={() => setInputType('text')}
-      >
-        <FontAwesomeIcon icon={faUpload} />
-        <span className={s.title}>Text</span>
-      </button>
+    <div className={s.container}>
+      <span className={s.buttonsContainer}>
+        <button
+          className={`${s.tabButton} ${s.left} ${inputType === 'upload' ? s.active : ''}`}
+          onClick={() => setInputType('upload')}
+        >
+          <FontAwesomeIcon icon={faUpload} />
+          <span className={s.title}>Upload</span>
+        </button>
+        <button
+          className={`${s.tabButton} ${s.middle} ${inputType === 'create' ? s.active : ''}`}
+          onClick={() => setInputType('create')}
+        >
+          <FontAwesomeIcon icon={faFileCirclePlus} />
+          <span className={s.title}>Create</span>
+        </button>
+        <button
+          className={`${s.tabButton} ${s.right} ${inputType === 'text' ? s.active : ''}`}
+          onClick={() => setInputType('text')}
+        >
+          <FontAwesomeIcon icon={faUpload} />
+          <span className={s.title}>Text</span>
+        </button>
+      </span>
     </div>
   );
 };
