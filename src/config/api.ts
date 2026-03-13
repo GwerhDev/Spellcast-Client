e60bc930b952fc2de6d69b1c2e737f00ce0a1279
GwerhDev <gwerh.dev@gmail.com>
2026-03-13 11:20:41 -0300

    feat: client side variables

--- src/config/api.ts
+++ src/config/api.ts
@@ -5,7 +5,7 @@ export const ACCOUNT_BASE = import.meta.env.VITE_ACCOUNT_URL;
 export const REDIRECT_LOGIN = import.meta.env.VITE_REDIRECT_LOGIN_URL;
 export const REDIRECT_SIGNUP = import.meta.env.VITE_REDIRECT_SIGNUP_URL;
 
-export const DB_NAME = import.meta.env.DB_NAME;
-export const DB_VERSION = import.meta.env.DB_VERSION; 
-export const DOCUMENTS_STORE_NAME = import.meta.env.DOCUMENTS_STORE_NAME;
-export const DOCUMENT_PROGRESS_STORE_NAME = import.meta.env.DOCUMENT_PROGRESS_STORE_NAME;
+export const DB_NAME = import.meta.env.VITE_DB_NAME;
+export const DB_VERSION = import.meta.env.VITE_DB_VERSION; 
+export const DOCUMENTS_STORE_NAME = import.meta.env.VITE_DOCUMENTS_STORE_NAME;
+export const DOCUMENT_PROGRESS_STORE_NAME = import.meta.env.VITE_DOCUMENT_PROGRESS_STORE_NAME;
