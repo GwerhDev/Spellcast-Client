a28e1ceedd99902f47513520ee4e48e879720161
GwerhDev <gwerh.dev@gmail.com>
2026-06-11 21:00:47 -0400

    style: normalizing environment

--- src/app/pages/Unauthorized.tsx
+++ src/app/pages/Unauthorized.tsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { RootState } from '../../store';
 import { UnauthorizedForm } from '../components/Forms/UnauthorizedForm';
-import { CLIENT_BASE, VITE_ENV, REDIRECT_LOGIN, APP_ID } from '../../config/api';
+import { CLIENT_BASE, ACCOUNT_BASE } from '../../config/api';
 
 export const Unauthorized = () => {
   const { logged } = useSelector((state: RootState) => state.session);
@@ -13,13 +13,9 @@ export const Unauthorized = () => {
     if (logged) {
       navigate('/');
     } else {
-      if (VITE_ENV !== "development") window.location.href = REDIRECT_LOGIN + "?callback=" + encodeURIComponent(CLIENT_BASE) + "&appId=" + APP_ID;
+      window.location.href = `${ACCOUNT_BASE}/login?callback=${encodeURIComponent(CLIENT_BASE)}`;
     }
   }, [logged, navigate]);
 
-  if (VITE_ENV === "development") return (
-    <div className="dashboard-sections">
-      <UnauthorizedForm />
-    </div>
-  )
+  return null;
 }
