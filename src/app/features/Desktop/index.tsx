68acbbdb55622b46a1eba173dd2ac1313eb47d26
GwerhDev <gwerh.dev@gmail.com>
2026-06-14 14:53:16 -0400

    style: normalize environment

--- src/app/features/Desktop/index.tsx
+++ src/app/features/Desktop/index.tsx
@@ -37,7 +37,9 @@ export const Desktop = () => {
   };
 
   return (
-    <div className={s.desktop} data-minimized={minimized} aria-hidden={!minimized}>
+    <>
+      <div className={s.desktop} data-minimized={minimized} aria-hidden={!minimized} />
+      <div className={s.launcherWrapper}>
       <AnimatePresence>
         {minimized && (
           <motion.div
@@ -85,6 +87,7 @@ export const Desktop = () => {
           </motion.div>
         )}
       </AnimatePresence>
-    </div>
+      </div>
+    </>
   );
 };
